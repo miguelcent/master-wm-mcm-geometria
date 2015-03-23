f3396f0f1ce10bcfbb055c53d41772ee819eab91
Miguel Centeno <miguelcent@gmail.com>
2015-03-23 12:06:12 +0100

    commit inicial

---
+++ src/Punto2D.java
@@ -0,0 +1,27 @@
+
+public class Punto2D {
+	
+	private float x;
+	private float y;
+	
+	public Punto2D(){
+		super();
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+}
