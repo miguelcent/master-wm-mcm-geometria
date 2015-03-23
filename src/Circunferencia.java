163fab1c117df09288861f448e49c1f8c8fbd8c7
Miguel Centeno <miguelcent@gmail.com>
2015-03-23 12:11:59 +0100

    Commit a circunferencia (rama)

---
+++ src/Circunferencia.java
@@ -0,0 +1,29 @@
+
+public class Circunferencia {
+
+	private Punto2D centro;
+	private float radio;
+	
+	public Circunferencia(){
+		super();
+	}
+
+	public Punto2D getCentro() {
+		return centro;
+	}
+
+	public void setCentro(Punto2D centro) {
+		this.centro = centro;
+	}
+
+	public float getRadio() {
+		return radio;
+	}
+
+	public void setRadio(float radio) {
+		this.radio = radio;
+	}
+	
+	
+	
+}
